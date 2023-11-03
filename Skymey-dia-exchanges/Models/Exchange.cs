68d3b47d1889c6cf0a0a7f2d0fda6ac89b541fb0
dotnetcsharp0x <dsotnikov1996@gmail.com>
2023-11-03 14:01:19 +0300

    Добавьте файлы проекта.

---
+++ Skymey-dia-exchanges/Models/Exchange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skymey_dia_exchanges.Models
+{
+    public class Exchange
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Volume24h { get; set; }
+        public int Trades { get; set; }
+        public int Pairs { get; set; }
+        public string Type { get; set; }
+        public string Blockchain { get; set; }
+    }
+}
---
+++ Skymey-dia-exchanges/Program.cs
@@ -0,0 +1,30 @@
+﻿using Nancy.Json;
+using RestSharp;
+using Skymey_dia_exchanges.Models;
+using System.Text;
+
+namespace Skymey_dia_exchanges
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                var client = new RestClient("https://api.diadata.org/v1/exchanges");
+                var request = new RestRequest("https://api.diadata.org/v1/exchanges", Method.Get);
+                request.AddHeader("Content-Type", "application/json");
+                var r = client.Execute(request).Content;
+                var Content = new StringContent(r.ToString(), Encoding.UTF8, "application/json");
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                Exchange[] ex = js.Deserialize<Exchange[]>(r);
+                foreach (Exchange ex2 in ex)
+                {
+                    Console.WriteLine(ex2.Name + " " + ex2.Blockchain);
+                }
+                int hours = 24;
+                Thread.Sleep(1000*60*60*hours);
+            }
+        }
+    }
+}
